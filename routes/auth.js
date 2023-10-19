e2925b18c722340709ead2180a01adcd8c07d37f
Sylas Falsy <enocksylas790@gmail.com>
2023-10-19 09:49:30 +0300

    installed express validator

--- routes/auth.js
+++ routes/auth.js
@@ -18,5 +18,47 @@ router.post(
   ],
   userRegister
 );
-
+router.post(
+  "/signin",
+  [check("emailAddress", "please enter a valid email").isEmail()],
+  async (req, res) => {
+    try {
+      const { emaiAddress, password } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(408).json({ error: errors.array() });
+      }
+      const user = await User.findOne({ emaiAddress });
+      if (!user) {
+        return res.status(400).json({
+          error: [
+            {
+              msg: "Invald Credentials",
+            },
+          ],
+        });
+      }
+      const checkpassword = await bcrypt.compare(password, user.password);
+      if (!checkpassword) {
+        return res.status(400).json({
+          error: [
+            {
+              msg: "Invalid credentials",
+            },
+          ],
+        });
+      }
+      const token = await jwt.sign(
+        { id: user._id, emaiAddressl: user.emaiAddress },
+        process.env.JWT_SIGN,
+        { expiresIn: "4rd" }
+      );
+      console.log(token);
+      const { password: userpassword, ...othres } = user.$getPopulatedDocs;
+      res.status(200).json({ ...othres, token });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+);
 module.exports = router;
