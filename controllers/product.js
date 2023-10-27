5a4a1a9b14ab1e351d81ff029fcd7bb5adf40c65
Sylas Falsy <enocksylas790@gmail.com>
2023-10-27 15:55:10 +0300

    changed outofstock to stock

--- controllers/product.js
+++ controllers/product.js
@@ -8,7 +8,7 @@ const addProduct = async (req, res) => {
       productImg,
       productDescription,
       productPrice,
-      outofStock,
+      Stock,
     });
     const savedProducts = await newProduct.save();
     res.status(201).json(savedProducts);
